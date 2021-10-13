72458b09c593aeadcc8dba5c8b94b376cf37ce3b
mastergamerjp <89471515+mastergamerjp@users.noreply.github.com>
2021-10-13 14:47:16 -0300

    Add files via upload

--- bingo.js
+++ bingo.js
@@ -1,17 +1,33 @@
 let listN = []
 
 function sortearNumero(){
-    let numeroSorteado = parseInt((Math.random()*30) + 1)
-    
-    if(listN.indexOf(numeroSorteado) == -1){
-        listN.push(numeroSorteado)
-        document.querySelector('.last-number').innerHTML = numeroSorteado
-        console.log(`Número novo! ${numeroSorteado}`)
-        let selected = document.querySelector(`#n${numeroSorteado}`)
-        selected.classList.add('active')
+    if(listN.length == 30){
+        let balls = document.querySelectorAll('.ball')
+        for(let i = 0; i < balls.length; i++){
+            let tempTag = balls[i]
+            tempTag.classList.remove('active')
+        }
+        listN = []
+
+        
     } else {
-        console.log(`Número repetido! ${numeroSorteado}`)
-        sortearNumero()
+        
+        let numeroSorteado = parseInt((Math.random()*30) + 1)
+    
+        if(listN.indexOf(numeroSorteado) == -1){
+            listN.push(numeroSorteado)
+            document.querySelector('.last-number').innerHTML = numeroSorteado
+            console.log(`Número novo! ${numeroSorteado}`)
+            let selected = document.querySelectorAll(`#n${numeroSorteado}`)
+            for(let i = 0; i < selected.length; i++){
+                selected[i].classList.add('active')
+            }
+            
+        } else {
+            console.log(`Número repetido! ${numeroSorteado}`)
+            sortearNumero()
+        }
+        console.log(listN)
     }
-    console.log(listN)
+    
 }
---
+++ fichas.js
@@ -0,0 +1,84 @@
+let players = []
+let listNFichas = []
+
+let modal = document.querySelector('.modal-box')
+
+function abrirModal(){modal.style.display = "flex"}
+function fecharModal(){modal.style.display = "none"}
+function adicionarJogador(){
+    let jogador = document.querySelector('#pessoas').value
+    let playerList = document.querySelector('ul.players')
+
+    if(players.indexOf(jogador) == -1 && jogador != ''){
+        playerList.innerHTML += `<li>${jogador}</li>`
+        players.push(jogador)
+
+        console.log(`Jogador "${jogador}" adicionado com sucesso!`)
+
+        console.log(`Lista de jogadores: ${players}`)
+    } else {
+        alert('Esse jogador já foi inserido, ou você não inseriu nenhum valor.')
+    }
+
+    document.querySelector('#pessoas').value = ''
+
+    document.querySelector('#pessoas').focus()
+}
+function gerarFichas(){
+    let campoFichas = document.querySelector('#fichas')
+    campoFichas.innerHTML = ''
+    listNFichas = []
+    //let listNFichas = []
+    for(let i = 0; i < players.length; i++){
+        let currentPlayer = players[i]
+        let seqNumbers = gerarNumerosFicha()
+        seqNumbers.sort()
+        listNFichas.push(seqNumbers)
+        campoFichas.innerHTML += `
+        <div class="ficha">
+            <h2>${currentPlayer}</h2>
+            <hr>
+            <table>
+                <tr>
+                    <td><div id="n${seqNumbers[0]}" class="ball">${seqNumbers[0]}</div></td>
+                    <td><div id="n${seqNumbers[1]}" class="ball">${seqNumbers[1]}</div></td>
+                    <td><div id="n${seqNumbers[2]}" class="ball">${seqNumbers[2]}</div></td>
+                    <td><div id="n${seqNumbers[3]}" class="ball">${seqNumbers[3]}</div></td>
+                </tr>
+                <tr>
+                    <td><div id="n${seqNumbers[4]}" class="ball">${seqNumbers[4]}</div></td>
+                    <td><div id="n${seqNumbers[5]}" class="ball">${seqNumbers[5]}</div></td>
+                    <td><div id="n${seqNumbers[6]}" class="ball">${seqNumbers[6]}</div></td>
+                    <td><div id="n${seqNumbers[7]}" class="ball">${seqNumbers[7]}</div></td>
+                </tr>
+                <tr>
+                    <td><div id="n${seqNumbers[8]}" class="ball">${seqNumbers[8]}</div></td>
+                    <td><div id="n${seqNumbers[9]}" class="ball">${seqNumbers[9]}</div></td>
+                    <td><div id="n${seqNumbers[10]}" class="ball">${seqNumbers[10]}</div></td>
+                    <td><div id="n${seqNumbers[11]}" class="ball">${seqNumbers[11]}</div></td>
+                </tr>
+            </table>
+        </div>
+        `
+        
+    }
+    players = []
+    document.querySelector('.players').innerHTML = ''
+    fecharModal()
+}
+
+function gerarNumerosFicha(){
+    let numbers = []
+    while(numbers.length < 12){
+        let currentNumber = parseInt((Math.random() * 30) + 1)
+        if(numbers.indexOf(currentNumber) == -1){
+            numbers.push(currentNumber)
+        }
+    }
+    if(listNFichas.indexOf(numbers) == -1){
+        return numbers
+    } else {
+        gerarNumerosFicha()
+    }
+    
+}
